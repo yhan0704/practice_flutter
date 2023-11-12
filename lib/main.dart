42d9fab4b420eae97edcae4b30391eecb09787b4
yhan0704 <hanyc1205@gmail.com>
2023-11-12 12:58:15 -0500

    getx reactive

--- lib/main.dart
+++ lib/main.dart
@@ -3,86 +3,62 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:practice/countController.dart';
 import 'package:practice/second.dart';
-import 'package:practice/view/album_view.dart';
+import 'package:practice/studentController.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final StudentController _con = Get.put(StudentController());
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Test Title"),
-      ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GetBuilder<CountController>(
-              init: CountController(),
-              builder: (_) =>
-                  Text("Current Count:${Get.find<CountController>().counter}"),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                TextButton(
-                  onPressed: () => Get.find<CountController>().increase(),
-                  child: const Text("증가"),
-                ),
-                TextButton(
-                  onPressed: () => Get.find<CountController>().decrease(),
-                  child: const Text("감소"),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Test Title"),
+          ),
+          body: ListView.builder(itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.all(15),
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  GetX<StudentController>(
+                    builder: (_) => Text(
+                      "ID: ${_con.studentList()[index]().studentId}, Name: ${_con.studentList()[index]().studentName}, Grade: ${_con.studentList()[index]().studentGrade}",
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () => _con.updateStudentName(
+                            _con.newStudentNames[index], index),
+                        child: const Text("이름 변경"),
+                      ),
+                      TextButton(
+                        onPressed: () => _con.updateStudentGrade(
+                            _con.newStudentGrades[index], index),
+                        child: const Text("성적 변경"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          })),
     );
   }
 }
