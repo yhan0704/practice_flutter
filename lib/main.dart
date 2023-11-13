8aecf7e94520993d57114c089023bf3e663994d2
yhan0704 <hanyc1205@gmail.com>
2023-11-13 18:48:54 -0500

    pagination:

--- lib/main.dart
+++ lib/main.dart
@@ -1,148 +1,149 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
+    return const MaterialApp(title: 'Flutter App', home: HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late final WebViewController _controller;
+class _HomePageState extends State<HomePage> {
+  final _url = 'https://jsonplaceholder.typicode.com/albums';
+  final int _limit = 20;
+  int _page = 1;
+  bool _hasNextPage = true;
+  bool _isFirstLoadRunning = false;
+  bool _isLoadMoreRunning = false;
+  List _albumList = [];
+
+  late ScrollController _controller;
 
   @override
   void initState() {
     super.initState();
+    _initLoad();
+    _controller = ScrollController()..addListener(_nextLoad);
+  }
 
-    late final PlatformWebViewControllerCreationParams params;
+  void _initLoad() async {
+    setState(() {
+      _isFirstLoadRunning = true;
+    });
 
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
+    try {
+      final res =
+          await http.get(Uri.parse("$_url?_page=$_page&_limit=$_limit"));
+      setState(() {
+        _albumList = json.decode(res.body);
+      });
+    } catch (e) {
+      print(e.toString());
     }
 
-    final WebViewController controller =
-        WebViewController.fromPlatformCreationParams(params);
+    setState(() {
+      _isFirstLoadRunning = false;
+    });
+  }
 
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            debugPrint('WebView is loading (progress : $progress%)');
-          },
-          onPageStarted: (String url) {
-            debugPrint('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            debugPrint('Page finished loading: $url');
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('''
-        
-        
-          
-                        Page resource error:
-        
-        
-          
-                          code: ${error.errorCode}
-        
-        
-          
-                          description: ${error.description}
-        
-        
-          
-                          errorType: ${error.errorType}
-        
-        
-          
-                          isForMainFrame: ${error.isForMainFrame}
-        
-        
-          
-                    ''');
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            debugPrint('allowing navigation to ${request.url}');
+  void _nextLoad() async {
+    if (_hasNextPage &&
+        !_isFirstLoadRunning &&
+        !_isLoadMoreRunning &&
+        _controller.position.extentAfter < 100) {
+      setState(() {
+        _isLoadMoreRunning = true;
+      });
+      _page += 1;
+      try {
+        final res =
+            await http.get(Uri.parse("$_url?_page=$_page&_limit=$_limit"));
+        final List fetchedPosts = json.decode(res.body);
 
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..addJavaScriptChannel(
-        'Toaster',
-        onMessageReceived: (JavaScriptMessage message) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(message.message)),
-          );
-        },
-      )
-      ..loadRequest(Uri.parse('https://flutter.dev/'));
+        if (fetchedPosts.isNotEmpty) {
+          setState(() {
+            _albumList.addAll(fetchedPosts);
+          });
+        } else {
+          setState(() {
+            _hasNextPage = false;
+          });
+        }
+      } catch (e) {
+        print(e.toString());
+      }
 
-    if (controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-
-      (controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
+      setState(() {
+        _isLoadMoreRunning = false;
+      });
     }
+  }
 
-    _controller = controller;
+  @override
+  void dispose() {
+    _controller.removeListener(_nextLoad);
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: WebViewWidget(controller: _controller),
+      appBar: AppBar(
+        title: const Text('ListView Pagination'),
       ),
+      body: _isFirstLoadRunning
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    controller: _controller,
+                    itemCount: _albumList.length,
+                    itemBuilder: (context, index) => Card(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 10),
+                      child: ListTile(
+                        title: Text(_albumList[index]['id'].toString()),
+                        subtitle: Text(_albumList[index]['title']),
+                      ),
+                    ),
+                  ),
+                ),
+                if (_isLoadMoreRunning == true)
+                  Container(
+                    padding: const EdgeInsets.all(30),
+                    child: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+                if (_hasNextPage == false)
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.blue,
+                    child: const Center(
+                      child: Text('No more data to be fetched.',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+              ],
+            ),
     );
   }
 }
---
+++ lib/studentModel.dart
@@ -0,0 +1,7 @@
+class Student {
+  int studentId;
+  String studentName;
+  String studentGrade;
+
+  Student({this.studentId = 0, this.studentName = "", this.studentGrade = ""});
+}
