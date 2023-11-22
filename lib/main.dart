403aeaafd87e5df021c5ebb6fde9255e8d4070b1
yhan0704 <hanyc1205@gmail.com>
2023-11-22 17:29:02 -0500

    start Task App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:practice/screens/tasks_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,110 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _current = 0;
-  final CarouselController _controller = CarouselController();
-
-  List imageList = [
-    "https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg",
-    "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_1280.jpg",
-    "https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830_1280.jpg",
-    "https://cdn.pixabay.com/photo/2015/06/19/20/13/sunset-815270_1280.jpg",
-    "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_1280.jpg",
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Test Title"),
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 300,
-            child: Stack(children: [
-              sliderWidget(), sliderIndicator()
-            ],),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text("Welcome to the carousel slide app",
-                style: TextStyle(fontSize: 18)),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget sliderWidget() {
-    return CarouselSlider(
-      carouselController: _controller,
-      items: imageList.map(
-        (imgLink) {
-          return Builder(
-            builder: (context) {
-              return SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Image(
-                  fit: BoxFit.fill,
-                  image: NetworkImage(
-                    imgLink,
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ).toList(),
-      options: CarouselOptions(
-        height: 300,
-        viewportFraction: 1.0,
-        autoPlay: true,
-        autoPlayInterval: const Duration(seconds: 4),
-        onPageChanged: (index, reason) {
-          setState(() {
-            _current = index;
-          });
-        },
-      ),
-    );
-  }
-
-  Widget sliderIndicator() {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: imageList.asMap().entries.map((entry) {
-          return GestureDetector(
-            onTap: () => _controller.animateToPage(entry.key),
-            child: Container(
-              width: 12,
-              height: 12,
-              margin:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color:
-                    Colors.white.withOpacity(_current == entry.key ? 0.9 : 0.4),
-              ),
-            ),
-          );
-        }).toList(),
-      ),
+      home: const TasksScreen(),
     );
   }
 }
--- lib/studentModel.dart
+++
@@ -1,7 +0,0 @@
-class Student {
-  int studentId;
-  String studentName;
-  String studentGrade;
-
-  Student({this.studentId = 0, this.studentName = "", this.studentGrade = ""});
-}
